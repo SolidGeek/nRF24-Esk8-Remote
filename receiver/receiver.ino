1d0b2e776600469be0d47caebe05c53a7389c5a5
MrSolidGeek <thesolidgeek@gmail.com>
2017-08-01 02:33:32 +0200

    Update receiver.ino

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -2,7 +2,8 @@
 #include <nRF24L01.h>
 #include "RF24.h"
 
-RF24 radio(7, 8);
+// 9 = CE-pin - 10 = CSN-pin
+RF24 radio(9, 10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 bool recievedData = false;
@@ -14,13 +15,12 @@ int speedPin = 5;
 byte gotByte = 1;
 
 void setup() {
-  Serial.begin(9600);
-
   radio.begin();
+  radio.setAutoAck(true);
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
   radio.openReadingPipe(1, pipe);
-
+  radio.setRetries(15, 15);
   radio.startListening();
 
   pinMode(speedPin, OUTPUT);
@@ -28,30 +28,31 @@ void setup() {
 }
 
 void loop() {
-
-  radio.writeAckPayload(pipe, &gotByte, sizeof(gotByte));
-
-  while (radio.available())
+  // If transmission is available
+  if (radio.available())
   {
+    // The next time a transmission is received on pipe, the data in gotByte will be sent back in the acknowledgement (this could later be changed to data from VESC!)
+    radio.writeAckPayload(pipe, &gotByte, sizeof(gotByte));
+
+    // Read the actual message
     radio.read(&motorSpeed, sizeof(motorSpeed));
     recievedData = true;
   }
 
   if (recievedData == true)
   {
+    // A speed is received from the transmitter (remote).
+    
     lastTimeReceived = millis();
     recievedData = false;
 
+    // Write the PWM signal to the ESC (0-255).
     analogWrite(speedPin, motorSpeed);
-
-    // Serial.println(motorSpeed);
   }
   else if ((millis() - lastTimeReceived) > timeoutMax)
   {
+    // No speed is received within the timeout limit.
     motorSpeed = 127;
     analogWrite(speedPin, motorSpeed);
-    
-    // Serial.println("Remote signal lost");
   }
-
 }
