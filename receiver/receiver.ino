aa2bcaa4f65a9c7b45f21584fa3a66c9fdd01c0e
MrSolidGeek <thesolidgeek@gmail.com>
2017-08-17 03:16:48 +0200

    Added UART communication with VESC

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,8 +1,15 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include "RF24.h"
+#include "VescUart.h"
 
-// 9 = CE-pin - 10 = CSN-pin
+struct vescValues {
+  float ampHours;
+  float inpVoltage;
+  long rpm;
+  long tachometerAbs;
+};
+
 RF24 radio(9, 10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
@@ -12,15 +19,19 @@ uint32_t lastTimeReceived = 0;
 int motorSpeed = 127;
 int timeoutMax = 500;
 int speedPin = 5;
-byte gotByte = 1;
+
+struct bldcMeasure measuredValues;
+
+struct vescValues data;
+unsigned long lastDataCheck;
 
 void setup() {
+  SERIALIO.begin(115200);
+
   radio.begin();
-  radio.setAutoAck(true);
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
   radio.openReadingPipe(1, pipe);
-  radio.setRetries(15, 15);
   radio.startListening();
 
   pinMode(speedPin, OUTPUT);
@@ -28,11 +39,14 @@ void setup() {
 }
 
 void loop() {
+
+  getVescData();
+
   // If transmission is available
   if (radio.available())
   {
     // The next time a transmission is received on pipe, the data in gotByte will be sent back in the acknowledgement (this could later be changed to data from VESC!)
-    radio.writeAckPayload(pipe, &gotByte, sizeof(gotByte));
+    radio.writeAckPayload(pipe, &data, sizeof(data));
 
     // Read the actual message
     radio.read(&motorSpeed, sizeof(motorSpeed));
@@ -42,7 +56,7 @@ void loop() {
   if (recievedData == true)
   {
     // A speed is received from the transmitter (remote).
-    
+
     lastTimeReceived = millis();
     recievedData = false;
 
@@ -54,5 +68,26 @@ void loop() {
     // No speed is received within the timeout limit.
     motorSpeed = 127;
     analogWrite(speedPin, motorSpeed);
+  }
+}
+
+void getVescData() {
+
+  if (millis() - lastDataCheck >= 250) {
+
+    lastDataCheck = millis();
+
+    // Only transmit what we need
+    if (VescUartGetValue(measuredValues)) {
+      data.ampHours = measuredValues.ampHours;
+      data.inpVoltage = measuredValues.inpVoltage;
+      data.rpm = measuredValues.rpm;
+      data.tachometerAbs = measuredValues.tachometerAbs;
+    } else {
+      data.ampHours = 0.0;
+      data.inpVoltage = 0.0;
+      data.rpm = 0;
+      data.tachometerAbs = 0;
+    }
   }
 }
