e41f273d6a31a22ad4d51128b86a079bc6c5f670
ModMiniMan <7424692+ModMiniMan@users.noreply.github.com>
2018-06-02 17:17:08 +0800

    Update receiver_VESC6.ino
    Used arduino servo library instead of analogwrite to send the signal to the VESC.
    
    Resolves the problem of VESC signal failure when throttle is adjusted rapidly

--- receiver_VESC6/receiver_VESC6.ino
+++ receiver_VESC6/receiver_VESC6.ino
@@ -2,6 +2,8 @@
 #include <nRF24L01.h>
 #include "RF24.h"
 #include "VescUart.h"
+#include <Servo.h>
+Servo myservo;
 
 //#define DEBUG
 
@@ -24,7 +26,7 @@ struct vescValues {
 
 RF24 radio(9, 10);
 //int radioChannel = 108; // Above most WiFi frequencies
-const uint64_t pipe = 0xA8A8F0F0E1LL;
+const uint64_t pipe = 0xA8A8FEFEE1LL; // Please change this to your own pipe address
 const uint8_t defaultChannel = 108;
 uint32_t timeoutTimer = 0;
 
@@ -37,6 +39,7 @@ struct callback {
   float inpVoltage;
   long rpm;
   long tachometerAbs;
+  float avgMotorCurrent;
 };
 
 struct bldcMeasure uartData;
@@ -45,7 +48,7 @@ struct callback returnData;
 bool recievedData = false;
 uint32_t lastTimeReceived = 0;
 
-int motorSpeed = 127;
+int motorSpeed = 1500 ; // This is in microseconds
 int timeoutMax = 500;
 int throttlePin = 5;
 //int resetTrigger = 0;
@@ -60,8 +63,10 @@ void setup() {
   SetSerialPort(&SERIALIO);
   SERIALIO.begin(115200);
 
-  pinMode(throttlePin, OUTPUT);
-  analogWrite(throttlePin, motorSpeed);
+  myservo.attach(throttlePin);
+  myservo.write(motorSpeed);
+  //pinMode(throttlePin, OUTPUT); // This is the old analogWrite method
+  //analogWrite(throttlePin, motorSpeed); // This is the old analogWrite method  
 
   initiateReceiver();
 }
@@ -88,24 +93,29 @@ void loop() {
     lastTimeReceived = millis();
     recievedData = false;
 
-    //radio.writeAckPayload(1, &returnData, sizeof(returnData));
+    // Write the PWM signal to the ESC (0-255) // This is the old analogWrite method
+    //analogWrite(throttlePin, motorSpeed); // This is the old analogWrite method
+    // Maps motorspeed signal from remote from 0-255 to 1000-2000ms
+    motorSpeed = map(motorSpeed, 0, 255, 1000, 2000);
+    // Writes microseconds to throttle pin
+    myservo.writeMicroseconds(motorSpeed);
 
-    // Write the PWM signal to the ESC (0-255).
-    analogWrite(throttlePin, motorSpeed);
   }
   else if ((millis() - lastTimeReceived) > timeoutMax)
   {
     // No speed is received within the timeout limit.
-    motorSpeed = 127;
-    analogWrite(throttlePin, motorSpeed);
+    //analogWrite(throttlePin, motorSpeed);
+    myservo.writeMicroseconds(1500);
   }
 }
 
-
 void initiateReceiver() {
 
+  // Start radio communication
   radio.begin();
   // radio.setChannel(defaultChannel);
+  radio.setDataRate(RF24_250KBPS);
+  //radio.setChannel(108);
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
   radio.openReadingPipe(1, pipe);
@@ -130,10 +140,11 @@ void getVescData() {
     }
     else
     {
-      data.ampHours       = 0.0;
-      data.inpVoltage     = 0.0;
-      data.rpm            = 0;
-      data.tachometerAbs  = 0;
+      data.ampHours         = 0.0;
+      data.inpVoltage       = 0.0;
+      data.rpm              = 0;
+      data.tachometerAbs    = 0;
+      data.avgMotorCurrent  = 0;
     }
   }
 }
