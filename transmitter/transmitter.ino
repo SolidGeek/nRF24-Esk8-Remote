a2c05ad36a26ab4214c5b4c9543ebd25e751fea5
JunwooHWANG <junwoo091400@gmail.com>
2018-07-06 09:33:20 +0900

    Correct uint64ToString for plain Decimal numbers
    previously, it was doing bit shift operation, and then truncated the string.
    but since the values given to this function won't go over 4 billion for sure, this won't make any difference really :(

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1053,13 +1053,16 @@ uint64_t generateAddress()
 
 String uint64ToString(uint64_t number)
 {
-	unsigned long part1 = (unsigned long)((number >> 32)); // Bitwise Right Shift
-	unsigned long part2 = (unsigned long)((number));
-
-	if(part1 == 0){
-	  return String(part2, DEC);
-	}
-	return String(part1, DEC) + String(part2, DEC);
+	String result = "";
+  	uint8_t base = 10;
+	
+	do {
+		char c = (input % base) + '0';
+		input /= base;
+		result += c;
+	} while (input);
+	
+  	return result;
 }
 
 String uint64ToAddress(uint64_t number)
